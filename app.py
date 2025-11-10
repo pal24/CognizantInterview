d2c593bca896be0e3de91c573d56bed342e793da
pal24 <pallavisingh941@gmail.com>
2025-11-10 13:23:00 +1100

    Add the updated file

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return jsonify(message="Hello from CI/CD Pipeline with Python!")
+    return jsonify(message="Test run of CI/CD Pipeline with Python!")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
